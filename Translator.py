74ca5268684c81ef8ae7cecb15ae10ca9e133954
Windows Master <94804675+windowsmaster09@users.noreply.github.com>
2023-04-26 10:37:54 +0300

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -17,7 +17,7 @@ def take_command():
     try:
         with sr.Microphone() as source:
             voice = listener.listen(source)
-            command = listener.recognize_google(voice, language="bg-BG")
+            command = listener.recognize_google(voice, language="None")#In place of None, put the initials of the speech recognition language. You will be able to see your initials here https://py-googletrans.readthedocs.io/en/latest/. After that, the program will accept English and the language you entered.
             command = command.lower()
     except:
         command = ""
@@ -45,7 +45,7 @@ def run_translator():
     f = open("chat.txt", "a+", encoding="utf-8")
     f.write(f"\nUser: {command}")
     f.close()
-    if "превод" in command or "translate" in command:
+    if "None" in command or "translate" in command:#Instead of None, you can put a word that is in the language you entered on line 22.
         print('listening...')
         talk('listening...')
         f = open("chat.txt", "a+", encoding="utf-8")
@@ -70,20 +70,20 @@ def run_translator():
         f = open("chat.txt", "a+", encoding="utf-8")
         f.write(f"\nUser: {command}")
         f.close()
-        if "repeat" in command or "повтори" in command:
+        if "repeat" in command or "None" in command:#Instead of None, you can put a word that is in the language you entered on line 22.
             print(command)
             talk(translation.text)
-        elif "spell out" in command or "изписване" in command:
+        elif "spell out" in command or "None" in command:#Instead of None, you can put a word that is in the language you entered on line 22.
             print(command)
             spelled_text = ", ".join(list(translation.text))
             talk(spelled_text)
             f = open("chat.txt", "a+", encoding="utf-8")
             f.write(f"\nBot: {spelled_text}")
             f.close()
-        elif "no" in command or "не" in command:
+        elif "no" in command or "None" in command:#Instead of None, you can put a word that is in the language you entered on line 22.
             print(command)
             run_translator()
-    elif "край" in command or "end" in command:
+    elif "None" in command or "end" in command:#Instead of None, you can put a word that is in the language you entered on line 22.
         exit()
 
 while True:
